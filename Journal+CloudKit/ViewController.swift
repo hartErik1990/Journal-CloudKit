239b4ddd76e9ee1fdd0298628d73e35f1936e64d
Erik Hartley <erik>
2018-02-08 13:39:07 -0700

    Initial Commit

---
+++ Journal+CloudKit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Journal+CloudKit
+//
+//  Created by Erik HARTLEY on 2/8/18.
+//  Copyright © 2018 Erik HARTLEY. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
